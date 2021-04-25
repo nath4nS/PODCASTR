38e2f8a2a73ec132dc1f9aec1b0b7eea75485e90
nath4nS <nathansousa360@gmail.com>
2021-04-25 17:26:29 -0300

    Quarto dia

--- podcastrnext/src/pages/episodes/[slug].tsx
+++ podcastrnext/src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
@@ -61,9 +62,25 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+          limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+      })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return {
         paths: [],
-        fallback: 'blocking'
+        fallback: true
     }
 }
 
